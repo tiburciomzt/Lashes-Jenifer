a270e3e753525c4e884f045f6d5fce362d79622a
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2021-12-10 03:22:43 -0600

    Agregando Backend, empezando por algunos pequeños ajustes

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+      <h1>Welcome</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header() {
+    return (
+        
+         <header>This is the Header</header>   
+        )
+}
+
+export default Header
