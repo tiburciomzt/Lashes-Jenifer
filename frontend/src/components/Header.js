637357a8d2f61486a14b10aa668f08bb3c0129bb
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2021-12-12 01:07:39 -0600

    Agregando barra de navegacion.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 function Header() {
     return (
-        
-         <header>This is the Header</header>   
-        )
+              <header className='text-center'>
+                <Navbar bg="light" variant="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand className="text-left" href="/">LashesJenifer</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/tecnicas"><i className='fas fa-certificate'></i>Tecnicas</Nav.Link>
+                            <Nav.Link href="/contacto"><i className='fas fa-envelope'></i>Conctacto</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                </header>
+         )
 }
 
 export default Header
