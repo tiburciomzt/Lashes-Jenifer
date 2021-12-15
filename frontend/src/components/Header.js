07e9d9ae64bdd3ee88eb1dfb7a00c4bcf9a24367
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2021-12-15 05:25:42 -0600

    Renderizando pagina con react-router-dom

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header className='text-center'>
             <Navbar bg="light" variant="light" expand="lg">
                 <Container>
-                    <Navbar.Brand className="text-left" href="/">LashesJenifer</Navbar.Brand>
+                    <Link to="/">
+                        <Navbar.Brand> LashesJenifer</Navbar.Brand>
+                    </Link>
+
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/tecnicas"><i className='fas fa-certificate'></i>Tecnicas</Nav.Link>
-                        <Nav.Link href="/contacto"><i className='fas fa-envelope'></i>Contacto</Nav.Link>
-                    </Nav>
+
+                        <Link to="/tecnicas">
+                        <Nav.Link href='/tecnicas'> <i className='fas fa-certificate'></i>Tecnicas</Nav.Link>
+                        </Link>
+                        
+                        <Link to="/contacto">
+                        <Nav.Link href='/contacto'> <i className='fas fa-envelope'></i>Contacto</Nav.Link>
+                        </Link>
+                    
+                </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
