417807178202e948ae4d707ad97e853308ea7dd7
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2021-12-15 05:24:31 -0600

    Agregando rutas para la app principal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
-
+import Tecnicaspage from './pages/Tecnicaspage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Detailtecnicascreen from './pages/Detailtecnicascreen';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <main>
+      <main className='py-3'>
         <Container>
-        <h1>Welcome</h1>
+          <Routes>
+            <Route path='/' element={<Tecnicaspage />} /> 
+            <Route path='/tecnica/:id' element={<Detailtecnicascreen />} /> 
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+      </BrowserRouter>
   );
 }
 
