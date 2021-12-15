02b1aa10d572c9a04e78c5acfb8d13b7d67f56b0
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2021-12-15 05:27:21 -0600

    renderizando links para ver a detalle la tecnica de pestaña

--- frontend/src/components/Tecnica.js
+++ frontend/src/components/Tecnica.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
+import { Link } from 'react-router-dom'
 
 function Tecnica({ tecnica }) {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/tecnica/${tecnica._id}`}>
+            <Link to={`/tecnica/${tecnica._id}`}>
                 <Card.Img src={tecnica.image} />
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`/tecnica/${tecnica._id}`}>
+                <Link to={`/tecnica/${tecnica._id}`}>
                     <Card.Title as="div">
                         <strong>{tecnica.name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
                 <Card.Text as="div">
                     <div className='my-2'>
                     <Rating value={tecnica.rating} text={`${tecnica.numReviews} reviews`} color={'#f8e825'} />
