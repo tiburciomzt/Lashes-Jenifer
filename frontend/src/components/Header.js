a04a3a0f0dace6029f1124e6f7cc04759c804c9a
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2021-12-15 03:47:57 -0600

    header componente para las vistas

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,20 +3,20 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 
 function Header() {
     return (
-              <header className='text-center'>
-                <Navbar bg="light" variant="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand className="text-left" href="/">LashesJenifer</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="/tecnicas"><i className='fas fa-certificate'></i>Tecnicas</Nav.Link>
-                            <Nav.Link href="/contacto"><i className='fas fa-envelope'></i>Conctacto</Nav.Link>
-                        </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                </header>
+        <header className='text-center'>
+            <Navbar bg="light" variant="light" expand="lg">
+                <Container>
+                    <Navbar.Brand className="text-left" href="/">LashesJenifer</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/tecnicas"><i className='fas fa-certificate'></i>Tecnicas</Nav.Link>
+                        <Nav.Link href="/contacto"><i className='fas fa-envelope'></i>Contacto</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
          )
 }
 
