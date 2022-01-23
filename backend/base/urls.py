c4c17d41223a6e8597e2277a15adee12b1c8edf5
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2022-01-22 23:49:36 -0600

    add url dinamico para cada producto

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('tecnicas/', views.getTecnicas, name="tecnicas"),
+    path('tecnicas/<str:pk>', views.getTecnica, name="tecnica")
 ]
